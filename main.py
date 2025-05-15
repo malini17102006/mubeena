cb9f2eed0492f717f49a8e9d811febbbd981388f
malini17102006 <23jubas18@annejac.ac.in>
2025-05-15 11:33:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# import streamlit as st
+# st.title("🤖My streamlit app🤖")
+# st.write("_My coding_")
+# st.write("**Python**")
+# st.write("# Mubeena")
+# st.write("## BCA")
+# st.write("### 2-nd Year")
+# st.write("- code")
+# st.write("new",color="green")
+# with st.sidebar:
+#   st.write("sfsg")
+#   st.divider()
+#   st.write("hgjajd")
+# st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe7CCNyNx75y702BomzCCwcZXekHtNL6swN1g8H1W_AQx0-qzGaM22BhY&s")
